220e2ae9cc73294777c404c774051bac08303486
seba_cj <diablo1050@gmail.com>
2017-07-18 21:24:46 +0200

    Building followers interface

--- src/manager/FollowManager.vue
+++ src/manager/FollowManager.vue
@@ -9,9 +9,14 @@
 					type="text"
 				/>
 			</div>
+			<div class="col-8">
+				<b-form-checkbox v-model="filters.state" value="following">Following</b-form-checkbox>
+				<b-form-checkbox v-model="filters.state" value="followback">Followback</b-form-checkbox>
+				<b-form-checkbox v-model="filters.state" value="nofollowing">Not following</b-form-checkbox>
+			</div>
 		</div>
 		<div class="row list">
-			<div v-for="user in users" class="user col-6 row">
+			<div v-for="user in clUsers" class="user col-6 row">
 				<div class="col-3">
 					<img :src="user.img"/>
 				</div>
@@ -24,8 +29,18 @@
 						<b-badge :variant="user.follows_me?'success':'danger'" :title="user.follows_me?'The user is following you':'The user is not following you'">Follower</b-badge>
 						<b-badge v-if="user.status == 'followback'" variant="info" title="You are following him back">Followback</b-badge>
 					</div>
-					<div>Actions</div>
+					<div>
+						<b-button-group size="sm">
+							<b-button v-if="user.status=='follow' || user.status=='followback'" variant="danger">Unfollow</b-button>
+							<b-button v-else variant="info">Follow</b-button>
+							<b-button variant="warning">Blacklist</b-button>
+							<b-button variant="">Whitelist</b-button>
+						</b-button-group>
+					</div>
 				</div>
+			</div>
+			<div v-if="!users.length">
+				Loading...
 			</div>
 		</div>
 	</div>
@@ -50,6 +65,10 @@
 		background-color: white;
 		border-radius: 3px;
 	}
+
+	button {
+		cursor: pointer;
+	}
 </style>
 
 <script>
@@ -60,7 +79,7 @@
 				users: [],
 				filters: {
 					name: "",
-					following: null
+					state: []
 				}
 			}
 		},
@@ -70,6 +89,17 @@
 		message (action, data) {
 			if (action == "usersData" && data.type == this.type)
 				this.users = data.list;
+		},
+		computed: {
+			clUsers () {
+				var arr = this.users;
+				if (this.filters.name)
+					arr = arr.filter((t) => {
+						return t.username.indexOf(this.filters.name) >= 0;
+					})
+
+				return arr;
+			}
 		}
 	}
 </script>
