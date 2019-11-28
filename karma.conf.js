ae20d84ddb19ac1caf78a5a55b0ad74f7090f074
Seba <sebacej.92@gmail.com>
2019-11-28 18:30:19 +0100

    [TESTING] Upgraded to webpack 4 as the old one is not compatible to recent test systems. Started to adapt configs and code to new system. Still some errors to fix

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,9 @@
 // Karma configuration
 // Generated on Thu Nov 28 2019 17:36:00 GMT+0100 (GMT+01:00)
+// Edited based on https://github.com/gabel/karma-webpack-example/blob/master/karma.conf.js
+
+var webpack = require("webpack"),
+    wconf = require("./webpack.config.js");
 
 module.exports = function(config) {
   config.set({
@@ -10,14 +14,13 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'requirejs', 'chai'],
+    frameworks: ['mocha', 'chai'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      'test-main.js',
-      { pattern: 'test/backend/*.js', included: false },
-      { pattern: 'test/frontend/*.js', included: false }
+      { pattern: 'test/backend/main.js', included: false },
+      { pattern: 'test/frontend/main.js', included: false }
     ],
 
 
@@ -25,10 +28,18 @@ module.exports = function(config) {
     exclude: [
     ],
 
+    plugins: [
+      require("karma-webpack")
+    ],
+
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      // only specify one entry point
+      // and require all tests in there
+      'test/backend/main.js': ['webpack'],
+      'test/frontend/main.js': ['webpack']
     },
 
 
@@ -36,6 +47,8 @@ module.exports = function(config) {
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
+
+    webpack: wconf,
 
 
     // web server port
--- test/backend/police.test.js
+++ test/backend/police.test.js
@@ -1,5 +1,7 @@
-describe('Array', function() {
-    describe('#indexOf()', function() {
+// Test Police js implementation
+
+describe('Police', function() {
+    describe('Test1', function() {
         it('should return -1 when the value is not present', function() {
             chai.assert.equal([1, 2, 3].indexOf(4), -1);
         });
