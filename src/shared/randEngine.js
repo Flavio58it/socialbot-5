7bea5c6274c377898c792872a73b8b54da224154
Seba <sebacej.92@gmail.com>
2019-12-11 13:35:45 +0100

    Fixed random engine buggy behavior

--- src/shared/randEngine.js
+++ src/shared/randEngine.js
@@ -1,11 +1,34 @@
 import {Random} from "random-js";
 
+/**
+ * 
+ * @param {integer} size : Number of ranbdom values in array
+ * @param {integer} min : Minimun value
+ * @param {integer} max : Maximum value
+ * 
+ * When the difference between min and max is less than the size the algorhytm cannot provide enough unique values in array. 
+ * The fallback for this is to provide only one value in array
+ */
+
 export default function (size, min, max) {
 	var arr = [];
-	if ((max - min) <= size)
+
+	// Fallback if not enough size. The system will resize the array to 1
+	if (((max - min) > 0 && (max - min) <= size) || ((max === min) && size > 0))
+		size = 1
+
+	// Block execution in cases where the random values will not be genuine
+	if (
+		(typeof min !== "number" || typeof max !== "number" || typeof size !== "number") ||
+		((max - min) < size && max !== min) 
+		|| (min > max)
+	)
 		throw new Error("Random array engine cannot work out this.");
+
+	// Create array of random unique numbers
 	while (arr.length < size) {
-		var rand = Random.integer(min, max)(Random.engines.browserCrypto);
+		var randEngine = new Random()
+		var rand = randEngine.integer(min, max);
 		if (arr.indexOf(rand) == -1) 
 			arr.push(rand);
 	}
--- test/backend/randomEngine.test.js
+++ test/backend/randomEngine.test.js
@@ -1,23 +1,29 @@
 import randomEngine from "../../src/shared/randEngine";
 
 describe("#RandomEngine()", function () {
-    it("Check without params", function () {
-        chai.expect(randomEngine()).to.be.a("array")
-        chai.expect(randomEngine()).to.have.lengthOf(0);
+    it("Check without params, should throw error", function () {
+        chai.expect(() => randomEngine()).to.throw("Random array engine cannot work out this.")
     });
 
-    it("Check with all to 0", function () {
-        chai.expect(() => randomEngine(0,0,0)).to.throw("Random array engine cannot work out this.")
+    it("Check with all to 0, should return empty array", function () {
+        chai.expect(randomEngine(0,0,0)).to.have.lengthOf(0)
     });
 
-    it("If one is requestsd from and to, 1 should be returned", function () {
+    it("Check with min and max to 0", function () {
+        chai.expect(randomEngine(2,0,0)).to.have.members([0])
+    });
+
+    it("If one is requested from and to, 1 should be returned", function () {
         var result = randomEngine(1,1,1);
-        chai.expect(result).to.be.a("array")
         chai.expect(result).to.have.members([1])
     });
 
     it("Check with from value that is greater to to value", function () {
         chai.expect(() => randomEngine(5, 5, 2)).to.throw("Random array engine cannot work out this.")
+    });
+
+    it("If too low max and min for size number fallback to size of 1", function () {
+        chai.expect(randomEngine(4, 1, 2)).to.have.lengthOf(1);
     });
 
     it("Check that the random value is correctly created", function () {
@@ -27,8 +33,8 @@ describe("#RandomEngine()", function () {
         chai.expect(random).to.have.lengthOf(200);
 
         random.forEach((value) => {
-            chai.expect(value).to.be.lessThan(300)
-            chai.expect(value).to.be.moreThan(3)
+            chai.expect(value).to.be.most(300)
+            chai.expect(value).to.be.at.least(3)
         })
     });
 })
